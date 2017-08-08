0eff20ae6c435bdba8f7ad8af9de1baa3b8aac43
Thomas Müller <thomas.mueller@de.ibm.com>
2017-08-08 14:31:47 +0000

    Spaces an der falschen Stelle sind doof.

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World from CloudFoundry!!\n');
-  }).listen(port);
+}).listen(port);
