1bba0c73d6a01c349c9baef1d62e26ccf6f8a6b7
Thomas Müller <thomas.mueller@de.ibm.com>
2017-08-08 14:08:58 +0000

    Ausrufezeichen sind wichtig!!!

--- app.js
+++ app.js
@@ -2,10 +2,14 @@
 
 /* This simple example just returns static content */
 
-var port = (process.env.VCAP_APP_PORT || 3000);
+var port = process.env.VCAP_APP_PORT || 3000;
 var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
+<<<<<<< Upstream, based on f7962f02d697a2e5ee1eb8febd694acfbaa6176e
   res.end('Hello World from CloudFoundry\n');
+=======
+  res.end('Hello World from CloudFoundry!!\n');
+>>>>>>> 004bde1 Ausrufezeichen sind wichtig!!!
 }).listen(port);
